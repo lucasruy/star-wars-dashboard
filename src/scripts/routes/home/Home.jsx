03119d4169e50a3191f45f9cb735958332c8811c
Lucas Ruy <lukas.r@hotmail.com>
2019-06-09 12:04:44 -0300

    chore(update): import Navigation component on Home Page

--- src/scripts/routes/home/Home.jsx
+++ src/scripts/routes/home/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import Navigation from 'components/navigation/Navigation.jsx'
+
 const Home = () => (
     <div>
+        <Navigation />
         Home Page...
     </div>
 )
