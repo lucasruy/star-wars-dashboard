4d9b0a44e93a1db536827479a5cf724cbf4852bf
Lucas Ruy <lukas.r@hotmail.com>
2019-06-09 12:03:05 -0300

    chore(update): added constants file on utils folder

---
+++ src/scripts/utils/CONSTANTS.js
@@ -0,0 +1,27 @@
+export default {
+    BREAKPOINT: {
+        MOBILE: '320px',
+        TABLET: '768px',
+        DESKTOP: '1280px'
+    },
+    COLOR: {
+        BLACK: '#272727',
+        GRAY_DARK: '#4D4D4D',
+        BLUE_LIGTH: '#80C6FF',
+        WHITE: '#FFFFFF',
+    },
+    FONT: {
+        NAME: {
+            RALEWAY: '"Raleway Sans", sans-serif',
+            ROBOTO: '"Roboto", sans-serif',
+        },
+        SIZE: {
+            DEFAULT: '16px',
+            MID: '32px',
+            BIG: '64px'
+        }
+    },
+    TRANSITION: {
+        DEFAULT: 'all 300ms ease'
+    }
+}
