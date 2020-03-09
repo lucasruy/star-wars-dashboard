bc19fde9111a86ca6f5f2b309e2e8f8d1d834cf6
Lucas Ruy <lukas.r@hotmail.com>
2020-03-09 15:01:47 -0300

    chore(svg): updated svg import for use new alias

--- src/scripts/components/Navigation/components/Logo.js
+++ src/scripts/components/Navigation/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import * as SVG from '../svg'
+import * as SVG from 'svg'
 
 const LogoStyled = styled.i`
   display: block;
--- src/scripts/components/Navigation/components/MenuList.js
+++ src/scripts/components/Navigation/components/MenuList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import * as SVG from 'svg'
 import { COLORS } from 'utils/CONSTANTS'
-import * as SVG from '../svg'
 
 const Menu = styled.ul`
     display: flex;
