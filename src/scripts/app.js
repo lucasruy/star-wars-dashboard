661585ed4abe69f12092cb24d39cbab26b88f01a
Lucas Ruy <lukas.r@hotmail.com>
2020-03-09 10:35:54 -0300

    feat(app): added global styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import { Home } from './pages/Home'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
+const Application = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Home />
+  </Fragment>
+)
+
 const AppNode = document.getElementById('react-app')
 
-ReactDOM.render(<Home />, AppNode)
+ReactDOM.render(<Application />, AppNode)
