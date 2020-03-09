93ceef4439f7e3a11c8c784dfcae721f976ccd22
Lucas Ruy <lukas.r@hotmail.com>
2020-03-09 11:31:23 -0300

    refactor(constants): changed export mode off constants file

--- src/scripts/utils/CONSTANTS.js
+++ src/scripts/utils/CONSTANTS.js
@@ -1,27 +1,30 @@
-export default {
-    BREAKPOINT: {
-        MOBILE: '320px',
-        TABLET: '768px',
-        DESKTOP: '1280px'
-    },
-    COLORS: {
-        BLACK: '#272727',
-        GRAY_DARK: '#4D4D4D',
-        BLUE_LIGTH: '#80C6FF',
-        WHITE: '#FFFFFF',
-    },
-    FONT: {
-        NAME: {
-            RALEWAY: '"Raleway Sans", sans-serif',
-            ROBOTO: '"Roboto", sans-serif',
-        },
-        SIZE: {
-            DEFAULT: '16px',
-            MID: '32px',
-            BIG: '64px'
-        }
-    },
-    TRANSITION: {
-        DEFAULT: 'all 300ms ease'
-    }
+export const BREAKPOINTS = {
+  MOBILE: '320px',
+  TABLET: '768px',
+  DESKTOP: '1024px',
+  DESKTOP_LARGE: '1280px'
+}
+
+export const COLORS = {
+  BLACK: '#272727',
+  GRAY_DARK: '#4D4D4D',
+  BLUE_LIGTH: '#80C6FF',
+  WHITE: '#FFFFFF',
+  TEXT: '#AFAFAF'
+}
+
+export const FONT = {
+  NAME: {
+    RALEWAY: '"Raleway Sans", sans-serif',
+    ROBOTO: '"Roboto", sans-serif',
+  },
+  SIZE: {
+    DEFAULT: '16px',
+    MID: '32px',
+    BIG: '64px'
+  }
+}
+
+export const TRANSITION = {
+  DEFAULT: 'all 300ms ease'
 }
